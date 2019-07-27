14ecc6ddd4bd07df4be7532873b2d26938ebca96
CyberBlast <ashishdantesparda@gmail.com>
2019-07-27 14:07:20 +0530

    07: Added unlockable doors which can be opened with dedicated keys

---
+++ Assets/Scripts/KeyLock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyLock : MonoBehaviour {
+
+	[SerializeField] GameObject key;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log("Collision occured.");
+		if (collision.gameObject == key)
+		{
+			Destroy(gameObject);
+			Destroy(collision.gameObject);
+		}
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,4 +9,25 @@ public class PlayerController : MonoBehaviour {
 			Destroy(other.gameObject);
 		}
 	}
+
+	void Update()
+	{
+		HandleInput();
+	}
+
+	private void HandleInput()
+	{
+		if (Input.GetMouseButton(0))
+		{
+			RaycastHit hit;
+			if (Physics.Raycast(transform.position, transform.forward, out hit, 2f))
+			{
+				if (hit.collider.gameObject.CompareTag("Key"))
+				{
+					GameObject key = hit.collider.gameObject;
+					key.transform.position = transform.position + transform.forward * 2f;
+				}
+			}
+		}
+	}
 }
