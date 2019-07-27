a90bf59bac4e4502a5da5039106f0d1d4c509881
CyberBlast <ashishdantesparda@gmail.com>
2019-07-27 13:11:20 +0530

    06: Added Pickups.

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+	
+	[SerializeField] float rotationSpeed;
+
+	void FixedUpdate () {
+		transform.Rotate(new Vector3(15, 30, 45) * Time.fixedDeltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Pickup"))
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
